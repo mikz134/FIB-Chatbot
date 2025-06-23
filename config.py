e5cd1a538b9861c21dce2279a960d3d7ab9d001b
mikz134 <73061026+mikz134@users.noreply.github.com>
2025-06-23 11:43:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ SECRET_KEY = 'development'
 # Read: https://flask-oauthlib.readthedocs.io/en/latest/client.html#lazy-configuration
 # Get them: https://raco.fib.upc.edu/api/v2/o/
 RACO = {
-    'consumer_key': 'btYurHdRXEoR5dcGc3W9v36fhEfk7jvrJWUIcO7K',
-    'consumer_secret': 'f6XtVQJ0SwwXU6qxOMNgEXy61xydxQWPh3neiiU6PCobl7poabZefBuWuUzTkLkLZiNrhP5d87tpABi9ULbltbTgzW2DYQhgn2zZCvdcKFVimemyln5msXrikGeEHbj3'
+    'consumer_key': '',
+    'consumer_secret': ''
 }
